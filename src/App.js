325ba943a8045354b832463ce04ff8d1115ba61e
Emanuele Bertuol <emanuelebertuol@gmail.com>
2021-03-15 00:57:06 +0100

    Adding packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
+// React Router Dom
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
+
     </>
   )
 }
