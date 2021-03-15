e732fad7ee118e824bf1bbef6f1a2bb9130f4f73
Emanuele Bertuol <emanuelebertuol@gmail.com>
2021-03-15 02:48:01 +0100

    Homepage, Login and Registration
    First Homepage, Login and Registration components

--- src/App.js
+++ src/App.js
@@ -4,9 +4,39 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// Components
+//Homepage
+import Homepage from "./components/Homepage/Homepage"
+// User authentication and registration
+import Login from "./components/Login/Login"
+import Registration from "./components/Registration/Registration"
+// Offline tools
+import Dutcher from "./components/OfflineTools/Dutcher/Dutcher"
+import Trimatcher from "./components/OfflineTools/Trimatcher/Trimatcher"
+
+
 function App() {
   return (
     <>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Homepage />
+          </Route>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
+          <Route path="/registration" exact>
+            <Registration />
+          </Route>
+          <Route path="/dutcher" exact>
+            <Dutcher />
+          </Route>
+          <Route path="trimatcher" exact>
+            <Trimatcher />
+          </Route>
+        </Switch>
+      </Router>
 
     </>
   )
